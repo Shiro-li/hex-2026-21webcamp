62e102654a843be8fff146fc6ba170cec42353fb
Shiro <shiro.littlewhite@gmail.com>
2026-07-05 10:41:04 +0800

    project and service RWD complete

--- project.js
+++ project.js
@@ -12,3 +12,20 @@ closebtn.addEventListener('click',() => {
     modal.classList.remove('active');
 });
 
+//首頁 nvbar
+
+const nvbarmenu = document.querySelector('.nvbar-m-menu');
+const nvbarclose = document.querySelector('.nvbar-m-close');
+const nvbarlist = document.querySelector('.nvbar-list');
+
+nvbarmenu.addEventListener('click', () => {
+    nvbarlist.classList.add('nvactive');
+    nvbarmenu.classList.add('remove');
+    nvbarclose.classList.add('nvactive');
+});
+
+nvbarclose.addEventListener('click', () => {
+    nvbarlist.classList.remove('nvactive');
+    nvbarclose.classList.remove('nvactive');
+    nvbarmenu.classList.remove('remove');
+});
--- service.js
+++ service.js
@@ -8,3 +8,21 @@ faqheader.forEach((item) => {
     currentItem.classList.toggle('faqactive');
   });
 });
+
+// 首頁nvbar
+
+const nvbarmenu = document.querySelector('.nvbar-m-menu');
+const nvbarclose = document.querySelector('.nvbar-m-close');
+const nvbarlist = document.querySelector('.nvbar-list');
+
+nvbarmenu.addEventListener('click', () => {
+    nvbarlist.classList.add('nvactive');
+    nvbarmenu.classList.add('remove');
+    nvbarclose.classList.add('nvactive');
+});
+
+nvbarclose.addEventListener('click', () => {
+    nvbarlist.classList.remove('nvactive');
+    nvbarclose.classList.remove('nvactive');
+    nvbarmenu.classList.remove('remove');
+});
